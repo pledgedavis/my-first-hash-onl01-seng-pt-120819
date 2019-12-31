f0ac218240168457bc5054aa5e3419c1efed81f2
pledgedavis <pledgedavis14@gmail.com>
2019-12-31 01:28:44 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -10,11 +10,19 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
+<<<<<<< HEAD
   the_manifest = { "whale bone corsets" => 5,
   "porcelain vases" => 2, 
   "oil paintings" =>3 }
  
 # the_manifest["oil paintings"]
+=======
+  the_manifest ={ "key1" => "value1",
+  "key2" => "value2", 
+  "key3" =>"value3" }
+ 
+the_manifest[3]
+>>>>>>> ef3cc2c8926c08e9faeeac8031a6664830c8514a
 end
 
 def retrieval
